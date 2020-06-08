85fcdd673b08641a229220ca18931858009ffcdd
Ameya <joshiameyajoshi@gmail.com>
2020-06-07 19:40:41 -0500

    FindMinSubarray: Optimized O(N) solution
    Description
    
    Signed-off-by: [Ameya Joshi]

--- GrokkingCode/SlidingWindow/FindMinSubArray.cpp
+++ GrokkingCode/SlidingWindow/FindMinSubArray.cpp
@@ -21,47 +21,54 @@ public:
     /* This function will calculate sum of contigous sub array and return true if it is greater
        than or equal to S
      */
-    bool SubArraySum(vector<int> &arr, int k, int S)
-    {
-        auto sum = 0;
-        auto wstart = 0; //Sliding window start
-
-        for (auto wend = 0; wend < arr.size(); ++wend)
-        {
-            sum += arr[wend];
-            // if the iteration has reached the end of window size k
-            // slide the window start by 1
-            if ((wend - wstart) >= k - 1)
-            {
-                // Store the max sum
-                if (sum >= S)
-                {
-                    // We found a contigous subarray which is equal to S
-                    // return true
-                    return true;
-                }
-
-                sum -= arr[wstart]; // subtract the start from the current window sum
-                ++wstart;           // increment window start
-            }
-        }
-
-        return false; // we have reached the end return false
-    }
-
     int findMinSubArray(vector<int> &arr, int S)
     {
-        // We are going to use maxSubArray algorithm to calculate maximum sum
-        // We start will min size window of 1 and keep increasing until array size
-        for (auto k = 1; k < arr.size(); ++k)
+
+        auto wstart = 0;                       //Sliding window start
+        auto wend = 0;                         // Sliding window end
+        auto minWindowLength = arr.size() + 1; // Start with minWindowLength equal to array size + 1
+        auto sum = 0;                          // Initialize sum to the first element of the array.
+
+        // while sum of window size 1 is >=S , record the min window length,
+        // shrink window by removing elements from the start
+        // Else Expand window from the end by increasing the wend
+        // Recalculate sum of the new window
+
+        while (wend < arr.size() && wstart < arr.size())
         {
-            if (SubArraySum(arr, k, S))
+            sum += arr[wend];
+            // cout << "DEBUG ELSE: wstart= " << wstart << endl;
+            // cout << "DEBUG ELSE: wend= " << wend << endl;
+            // cout << "DEBUG ELSE: Sum= " << sum << endl;
+            while (sum >= S)
             {
-                return k;
+                // We found a contigous subarray which is equal to S
+                // calculate window size
+                auto windowSize = (wend - wstart) + 1;
+
+                // cout << "DEBUG WHILE: window size= " << windowSize << endl;
+                // cout << "DEBUG WHILE: Sum= " << sum << endl;
+                // cout << "DEBUG WHILE: wstart= " << wstart << endl;
+                // cout << "DEBUG WHILE: wend= " << wend << endl;
+
+                if (windowSize < minWindowLength)
+                {
+                    minWindowLength = windowSize;
+                }
+
+                sum -= arr[wstart];
+                // cout << "DEBUG WHILE AFTER: Sum= " << sum << endl;
+                ++wstart; //keep shrinking window size
             }
+
+            ++wend; // increase window size by sliding right
         }
 
-        return 0;
+        if (minWindowLength == arr.size() + 1)
+        {
+            return 0;
+        }
+        return minWindowLength; // we have reached the end return false
     }
 };
 
@@ -69,12 +76,23 @@ int main(int argc, char const *argv[])
 {
     Solution soln = Solution();
     vector<int> arr = {2, 1, 5, 2, 3, 2};
+    vector<int> arr2 = {2, 1, 5, 2, 8};
+    vector<int> arr3 = {3, 4, 1, 1, 6};
 
     auto S = 7;
-    auto smallSubArray = soln.findMinSubArray(arr, S);
+    auto S2 = 8;
 
     cout << " Smallest contigous subarray whose sum is greater than or equal to " << S
-         << " is of size: " << smallSubArray << endl;
+         << " is of size: " << soln.findMinSubArray(arr, S)
+         << endl;
+
+    cout << " Smallest contigous subarray whose sum is greater than or equal to " << S
+         << " is of size: " << soln.findMinSubArray(arr2, S)
+         << endl;
+
+    cout << " Smallest contigous subarray whose sum is greater than or equal to " << S
+         << " is of size: " << soln.findMinSubArray(arr3, S2)
+         << endl;
 
     return 0;
 }
