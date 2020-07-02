27f572faeb26e0213c770a9d8c884e9909a07d88
Ameya <joshiameyajoshi@gmail.com>
2020-07-01 20:08:29 -0500

    O(N) algorithm to detect if the given linked list is palindrome or not
    Signed-off-by: [Ameya Joshi]

--- GrokkingCode/TwoPointers/LinkedListMiddle.cpp
+++ GrokkingCode/TwoPointers/LinkedListMiddle.cpp
@@ -1,5 +1,4 @@
 /* Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
-
 If the total number of nodes in the LinkedList is even, return the second middle node. */
 
 #include <iostream>
@@ -20,7 +19,21 @@ public:
 class LinkedListMiddle
 {
 private:
-    /* data */
+    static ListNode *reverse(ListNode *head)
+    {
+        // Reverse the linked list from middle->next to null
+        ListNode *prev = nullptr;
+        while (head != nullptr)
+        {
+            auto next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+
 public:
     static ListNode *findMiddle(ListNode *head)
     {
@@ -36,6 +49,69 @@ public:
         // incase of even length linked list the slow will point to middle->next node
         // incase of odd length linkedlist the slow will point to middle node
         return slow;
+    }
+
+    /* Palindrome LinkedList (medium) 
+    Given the head of a Singly LinkedList, write a method to check if the LinkedList is a palindrome or not.
+    Your algorithm should use constant space and the input LinkedList should be in the original 
+    form once the algorithm is finished. The algorithm should have O(N)O(N)O(N) time complexity  
+    where ‘N’ is the number of nodes in the LinkedList.*/
+
+    static bool isPalinDrome(ListNode *head)
+    {
+        auto isPalindrome = true;
+        if (head == nullptr)
+        {
+            return false;
+        }
+        // Find the middle of the linked list
+        auto middle = findMiddle(head);
+
+        if (middle->next == nullptr)
+        {
+            return (head->val == middle->val);
+        }
+
+        // Reverse the linked list from middle->next to null
+        middle->next = reverse(middle->next);
+
+        // if the linked list is a palindrome then node values from head-->middle
+        // and middle-->>null will be same of reversed linked list
+        // in case of even length linked list node before middle should be same as middle
+        auto ptr1 = head;
+        auto ptr2 = middle->next;
+        while (ptr2 != nullptr)
+        {
+            // if the node values are different the linked list is not a palindrome
+            if (ptr1->val != ptr2->val)
+            {
+                isPalindrome = false;
+                break;
+            }
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+
+        // at the end of the above while loop the ptr1 should point to middle
+        // if it is odd length linked list or if its even length linked list
+        // then ptr1->next should be equal to middle
+        if (ptr1 && ptr1->next == middle)
+        {
+            // if even length list then ptr1->next is middle in that case
+            // ptr1->val should be equal to middle->value
+            // as in case of even length the middle value returned is
+            // (N/2) + 1
+            isPalindrome &= (ptr1->val == middle->val);
+        }
+        else
+        {
+            isPalindrome &= (ptr1 == middle);
+        }
+
+        //Reverse the linked list after middle to restore the original list
+        middle->next = reverse(middle->next);
+
+        return isPalindrome;
     }
 };
 
@@ -53,4 +129,13 @@ int main()
     auto middle = LinkedListMiddle::findMiddle(head);
 
     std::cout << "Linked List Middle node is: " << middle->val << endl;
+
+    ListNode *head2 = new ListNode(2);
+    head2->next = new ListNode(4);
+    head2->next->next = new ListNode(6);
+    head2->next->next->next = new ListNode(6);
+    head2->next->next->next->next = new ListNode(4);
+    head2->next->next->next->next->next = new ListNode(2);
+
+    std::cout << "Linked List is Palindrome? " << (LinkedListMiddle::isPalinDrome(head2) ? "TRUE" : "FALSE") << endl;
 }
