464fa0361c07026215466117897712768589f87e
Ameya Joshi <joshiameyajoshi@gmail.com>
2021-12-03 00:22:26 -0600

    Added comments explaining the algorithm for sliding window char replacement

--- LeetCode/CharReplacement.cpp
+++ LeetCode/CharReplacement.cpp
@@ -13,9 +13,15 @@ static int characterReplacement(string s, int k)
 
     for(auto end = 0 ; end < s.length(); ++end)
     {
-        char_map[s[end] - 'A']++;
+        char_map[s[end] - 'A']++; // increase char frequency
         auto window = (end - start) + 1;
         auto max_iter = max_element(char_map.begin(),char_map.end());
+
+        // within the window calculate the diff between window length 
+        // and char that has max repetition frequency, if its greater 
+        // than k, then start reducing window from the left, else if its 
+        // less than K then within this window i can replace k chars to
+        // get max window.
         if(window - *max_iter > k)
         {
             while(window - *max_iter > k)
@@ -34,7 +40,10 @@ static int characterReplacement(string s, int k)
 
 int main(int argc, char const *argv[])
 {
-    auto ans = characterReplacement("AAA",0);
-    cout << ans << endl;
+    cout << characterReplacement("AAA",0) << endl;
+    cout << characterReplacement("SDCABAABBDDDEF",2) << endl;
+    cout << characterReplacement("ABBB",1) << endl;
+    cout << characterReplacement("ABAB",2) << endl;
+    cout << characterReplacement("ABCDEFA",2) << endl;
     return 0;
 }
