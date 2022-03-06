21edfc3f1278058e2f33975df728d5bcbca2077d
Ameya Joshi <joshiameyajoshi@gmail.com>
2022-03-06 11:41:30 -0600

    Finalized implementation of LFUcache, moved to using list splice instead of insert hen erase.

--- HackerRank/LFUCache.cpp
+++ HackerRank/LFUCache.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <map>
-#include <string>
+#include <unordered_map>
 #include <list>
-#include <fstream>
+#include <memory>
 using namespace std;
 
 struct Node
@@ -17,11 +17,11 @@ class Cache
 {
 
 protected:
-    map<int, list<Node>::iterator> mp; // map the key to the node in the linked list
-    int cp;                            // capacity
-    list<Node> cache_list;             // cache list
-    virtual void set(int, int) = 0;    // set function
-    virtual int get(int) = 0;          // get function
+    unordered_map<int, list<Node>::iterator> mp; // map the key to the node in the linked list
+    int cp;                                      // capacity
+    list<Node> cache_list;                       // cache list
+    virtual void set(int, int) = 0;              // set function
+    virtual int get(int) = 0;                    // get function
 public:
     Cache(int in_cp) : cp(in_cp), cache_list(), mp(){};
 };
@@ -30,7 +30,51 @@ class LFUCache : public Cache
 {
 private:
     int num_keys;
-    map<int, list<Node>::iterator> freq_mp; // this will keep track of freq to node pointer
+    unordered_map<int, list<Node>::iterator> freq_mp; // this will keep track of freq to node pointer
+
+    // this function will position the found node in the list with the key
+    // in appropriate position with respect to its frequency
+    void __update_list(unordered_map<int, list<Node>::iterator>::iterator &found)
+    {
+        auto curr_count = found->second->count;
+        ++found->second->count; // increment the count
+
+        // check if you have already a list element with curr_count + 1
+        // this node now shall be inserted before that
+        auto insert_iter = freq_mp.find(found->second->count);
+        // else it will be inserted before the node that has count = curr_count;
+        auto it = freq_mp.find(curr_count);
+        auto lfu_iter = it->second;
+        // check if the iterator node returned from that freqmap matches the
+        // key we are updating , that means for the old freq count the mapped
+        // list pointer needs an udpate
+        if (lfu_iter->key == found->second->key)
+        {
+            lfu_iter++;
+
+            if (lfu_iter == cache_list.end() || lfu_iter->count != curr_count)
+            {
+                // means no nodes exist with that count anymore
+                // erase from frequency map
+                freq_mp.erase(curr_count);
+            }
+            else
+            {
+                freq_mp[curr_count] = lfu_iter;
+            }
+        }
+
+        if (insert_iter != freq_mp.end())
+        {
+            cache_list.splice(insert_iter->second, cache_list, found->second);
+        }
+        else
+        {
+            cache_list.splice(lfu_iter, cache_list, found->second);
+        }
+
+        freq_mp[found->second->count] = found->second;
+    }
 
 public:
     LFUCache(int capacity) : Cache(capacity),
@@ -44,33 +88,31 @@ public:
         auto found = mp.find(key);
         if (found != mp.end())
         {
-            auto erase_iter = found->second;
-
-            Node nc(key, val, erase_iter->count + 1);
-            auto insert_iter = freq_mp.find(nc.count);
-            if (insert_iter != freq_mp.end())
-            {
-                insert_iter->second = cache_list.insert(insert_iter->second, nc);
-                mp[key] = insert_iter->second;
-            }
-            else
-            {
-                auto insert_iter = freq_mp.find(erase_iter->count);
-                auto iter = cache_list.insert(insert_iter->second, nc);
-                freq_mp[nc.count] = iter;
-                mp[key] = iter;
-            }
-            cache_list.erase(erase_iter);
-            if (erase_iter == freq_mp[erase_iter->count])
-            {
-                freq_mp.erase(erase_iter->count);
-            }
-
+            found->second->value = val;
+            __update_list(found);
         }
         else
         {
             Node nc(key, val, 1);
+            // delete keys if num_keys greater than capacity
+            if (num_keys >= cp)
+            {
+                --num_keys;
+                auto it = mp[cache_list.back().key];
+
+                // if the iterator is mapped to the head
+                // of the list for that count, delete it from
+                // freq_map
+                if (it == freq_mp[it->count])
+                {
+                    freq_mp.erase(it->count);
+                }
+
+                mp.erase(cache_list.back().key);
+                cache_list.pop_back();
+            }
             ++num_keys;
+
             auto insert_iter = freq_mp.find(nc.count);
 
             if (insert_iter != freq_mp.end())
@@ -84,22 +126,6 @@ public:
                 freq_mp[nc.count] = prev(cache_list.end());
                 mp[key] = prev(cache_list.end());
             }
-
-            if (num_keys > cp)
-            {
-                auto it = mp[cache_list.back().key];
-
-                if (freq_mp.find(it->count) != freq_mp.end())
-                {
-                    if (it == freq_mp[it->count])
-                    {
-                        freq_mp.erase(it->count);
-                    }
-                }
-                mp.erase(cache_list.back().key);
-                cache_list.pop_back();
-                --num_keys;
-            }
         }
     }
 
@@ -109,28 +135,8 @@ public:
         auto found = mp.find(key);
         if (found != mp.end())
         {
-            auto erase_iter = found->second;
-            ret_val = erase_iter->value;
-            Node nc(key, ret_val, erase_iter->count + 1);
-            auto insert_iter = freq_mp.find(nc.count);
-            if (insert_iter != freq_mp.end())
-            {
-                insert_iter->second = cache_list.insert(insert_iter->second, nc);
-                mp[key] = insert_iter->second;
-            }
-            else
-            {
-                auto insert_iter = freq_mp.find(erase_iter->count);
-                auto iter = cache_list.insert(insert_iter->second, nc);
-                freq_mp[nc.count] = iter;
-                mp[key] = iter;
-            }
-            cache_list.erase(erase_iter);
-            if (erase_iter == freq_mp[erase_iter->count])
-            {
-                freq_mp.erase(erase_iter->count);
-            }
-
+            ret_val = found->second->value;
+            __update_list(found);
         }
 
         return ret_val;
@@ -139,21 +145,19 @@ public:
 
 int main()
 {
-    LFUCache ca(4);
-
-    ca.set(1, 2);
-    ca.set(2, 3);
-    ca.set(3, 5);
-    ca.set(1, 3);
-    cout << ca.get(1) << endl;
-    ca.set(10, 8);
-    ca.set(2, 5);
-    ca.set(3, 6);
-    cout << ca.get(2) << endl;
-    cout << ca.get(2) << endl;
-    ca.set(11, 12);
-    cout << ca.get(10) << endl;
-
+    unique_ptr<LFUCache> ca = make_unique<LFUCache>(2);
+    ca->set(1, 1);
+    ca->set(2, 2);
+    cout << ca->get(1) << endl;
+    ca->set(3, 3);
+    cout << ca->get(2) << endl;
+    cout << ca->get(3) << endl;
+    ca->set(4, 4);
+    cout << ca->get(1) << endl;
+    cout << ca->get(3) << endl;
+    cout << ca->get(4) << endl;
+    ca->set(5, 6);
+    cout << ca->get(3) << endl;
 
     return 0;
 }
